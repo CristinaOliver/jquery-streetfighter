6d2f003feba6b786952cda257ee8058d1ccdc90b
CristinaOliver <cristina.oliverduran@gmail.com>
2016-07-31 20:49:39 +0200

    jquery-streetfighter exercise

---
+++ js/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+    $('.ryu')
+    .mouseenter(function() {
+        $('.ryu-action').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+    	$('.ryu-action').hide();
+        $('.ryu-still').show();
+        
+    })
+    .mousedown(function() {
+        playHadouken();
+        $('.ryu-action').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show()
+            .animate({
+                    'left': '1020px'
+                },
+                500,
+                function() {
+                    $('.hadouken').hide();
+                    $('.hadouken').css('left', '520px');
+                });
+    })
+    .mouseup(function() {
+        $('.ryu-action').hide();
+        $('.ryu-ready').show();
+
+    });
+
+    $(document).keydown(function(e) {
+            if (e.keyCode == 88) {
+                $('.ryu-action').hide();
+                $('.ryu-cool').show();
+            }
+        })
+        .keyup(function() {
+            $('.ryu-still').show();
+            $('.ryu-cool').hide();
+        });
+
+    function playHadouken() {
+        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].load();
+        $('#hadouken-sound')[0].play();
+    }
+
+});
